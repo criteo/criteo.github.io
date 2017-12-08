2f81729c115d401b23677381e0a1d791ae0b5c32
Sam Verhasselt <s.verhasselt@criteo.com>
2017-12-08 15:57:29 -0800

    Using new source for language icons.

--- js/init.js
+++ js/init.js
@@ -1,33 +1,30 @@
 (function($){
   $(function(){
-
   	var createRepoCard = function(repo) {
   		var card = $(".repoCard").clone();
   		card.find(".card-title").text(repo.name);
   		card.find(".repo_link").attr("href", repo.html_url);
   		card.find(".project-description").text(repo.description || " ");
-  		// card.find(".language").text(repo.language);
   		card.find(".star_count").text(repo.stargazers_count);
   		card.find(".watchers_count").text(repo.watchers_count)
   		addContributors(repo, card);
-      addLanguageSvg(repo.language, card);
+		  card.find(".language").addClass("devicon-"+ getRepoLanguage(repo) + "-plain")
   		card.removeClass("hide repoCard");
   		return card;
-  	},
-  		addContributors = function(repo, card) {
-  			$.get(repo.contributors_url, {"access_token" : "c1ed8f00d37bcd5b91fb80b07dc149b10f5f7e35"}, function(people) {
-  				people = people || [];
-  				people.sort(function(a,b) {return b.concontributions - a.concontributions;})
-  				.forEach(function(person,index){
-  					card.find(".contributors").append(
-  						'<div class="chip">' +
-            			'<img src="'+person.avatar_url +
-            			'" alt="Contact Person">' + person.login +
-       					'</div>'
-       				);
-  				});
-  			});
-  			
+  },
+    addContributors = function(repo, card) {
+      $.get(repo.contributors_url, {"access_token" : "c1ed8f00d37bcd5b91fb80b07dc149b10f5f7e35"}, function(people) {
+        people = people || [];
+        people.sort(function(a,b) {return b.concontributions - a.concontributions;})
+          .forEach(function(person,index){
+            card.find(".contributors").append(
+              '<div class="chip">' +
+              '<img src="'+person.avatar_url +
+              '" alt="Contact Person">' + person.login +
+              '</div>'
+            );
+          });
+      });
 	},
 		populateRepositoryTab = function(repoName, tabId) {
 			$.get("//api.github.com/users/"+ repoName + "/repos", 
@@ -36,33 +33,21 @@
 		    	repos.sort(function(repo1, repo2) {
 		    		return repo2.stargazers_count - repo1.stargazers_count;
 		    	}).forEach(function(repo,index){
-		    		$("#" + tabId + " .repositories"+ index % 3).append(createRepoCard(repo));
+            if (!repo.name.includes("criteo.github.io")) {
+              $("#" + tabId + " .repositories"+ index % 3).append(createRepoCard(repo));
+            }
 		    	});
 		    });
 	},
-    addLanguageSvg = function(repoLanguage, card) {
-      repoLanguage = repoLanguage ? repoLanguage.toLowerCase(): "";
-      $.ajax({ 
-        url: "https://wrapapi.com/use/saym/devicon/svg/latest?wrapAPIKey=KeAIaBXYj5SfQZvZ0rzHd7vgCzVyZvI3&name="+repoLanguage,
-        method: "GET",
-        dataType: "json",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "*"
-        },
-        success: function(data) {
-            card.find(".card-content").append($("<div></div>")
-              .addClass("left-align")
-              .append(data.data.svg)
-            );
-          }
-      });
+    getRepoLanguage = function(repo) {
+      if (repo.language == null) return "";
+      return repo.language.toLowerCase().replace("#", "sharp");
   };
 
 	$("#copyright").text(new Date().getFullYear());
-    $('.button-collapse').sideNav();
-    populateRepositoryTab("criteo", "user_criteo");
-    //populateRepositoryTab("criteo-forks", "user_criteo_forks");
-    populateRepositoryTab("criteo-cookbooks", "user_criteo_cookbooks");
+  $('.button-collapse').sideNav();
+  populateRepositoryTab("criteo", "user_criteo");
+  //populateRepositoryTab("criteo-forks", "user_criteo_forks");
+  populateRepositoryTab("criteo-cookbooks", "user_criteo_cookbooks");
   }); // end of document ready
 })(jQuery); // end of jQuery name space
